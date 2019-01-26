b084b97c8fbb3590774bfcbc3040eef901f765c5
zmichgen <zmichgen@gmail.com>
2019-01-26 14:47:30 +0300

    Updates

---
+++ precache-manifest.c15bbe62623f88cf4b560feae1ad464b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c88ac452e4d55a303c6",
+    "url": "/codejam5/static/js/runtime~main.5c88ac45.js"
+  },
+  {
+    "revision": "e366db2e8afba1113eeb",
+    "url": "/codejam5/static/js/main.e366db2e.chunk.js"
+  },
+  {
+    "revision": "8d38e53d4efd7a8d48b9",
+    "url": "/codejam5/static/js/1.8d38e53d.chunk.js"
+  },
+  {
+    "revision": "e366db2e8afba1113eeb",
+    "url": "/codejam5/static/css/main.067cd07e.chunk.css"
+  },
+  {
+    "revision": "8d38e53d4efd7a8d48b9",
+    "url": "/codejam5/static/css/1.0fb4baa2.chunk.css"
+  },
+  {
+    "revision": "e6eb8ac605c5c6fc145173fb2dca21a0",
+    "url": "/codejam5/index.html"
+  }
+];
