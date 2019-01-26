8f1ff47b604f817a5e5b63c49a4d0d8238567459
zmichgen <zmichgen@gmail.com>
2019-01-26 19:14:20 +0300

    fix: recover after filure

--- src/App.js
+++ src/App.js
@@ -1,45 +1,20 @@
 import React, { Component } from 'react';
-import './App.css';
+import * as _ from 'lodash';
+import './App.scss';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Data from './codejam5.json';
+import English from './england.jpg';
+import Russian from './russia.jpg';
+import Belarus from './belarus.jpg';
 
 const Main = (props) => {
-  const header = {
-    ru: 'Главная',
-    by: 'Галоуная',
-    en: 'Main'
-  }
-  const { lang } = props;
-  return () => {return <h1>{header[lang]}</h1>};
+  console.log(props);
+  const header = _.keys(props)[1];
+  return () => {return <h1>{header}</h1>};
 }
 const Search = (props) => {
-  const header = {
-    ru: 'Поиск',
-    by: 'Пошук',
-    en: 'Search'
-  }
-  const { lang } = props;
-  return () => {return <h1>{header[lang]}</h1>};
-}
-
-const Menu = {
-  ru: {
-    main:'Главная',
-    search: 'Поиск',
-    lang: 'Русский',
-    brand: 'Белорусские писатели'
-  },
-  by: {
-    main:'Галоуная',
-    search: 'Пошук',
-    lang: 'Беларуская',
-    brand: 'Беларускія пісьменнікі'
-  },
-  en: {
-    main:'Main',
-    search: 'Search',
-    lang: 'English',
-    brand: 'Belarusian writers'
-  }  
+  const header = _.keys(props)[0]
+  return () => {return <h1>{header}</h1>};
 }
 
 class App extends Component {
@@ -48,51 +23,48 @@ class App extends Component {
     lang: 'ru'
   }
 
-  switchLanguage = () => {
-    let { lang } = this.state; 
-    switch (lang) {
-      case 'ru': {lang = 'by'; break;}
-      case 'by': {lang = 'en'; break;}
-      case 'en': {lang = 'ru'; break;}
-      default: break;
-    }
+  switchLanguage = (e) => {
+    const lang = e.target.id;
     this.setState({lang: lang});
   }
 
   render() {
-    const {main, search, lang, brand} = Menu[this.state.lang];
-    const mainPage = Main(this.state);
-    const searchPage = Search(this.state);
+    const menuItems = _.values(Data[this.state.lang].interface.mainMenu);    
+    const mainPage = Main(Data[this.state.lang].autors);
+    const searchPage = Search(Data[this.state.lang].autors);
     return (
       <Router basename="/codejam5" >
         <div className="wrapper">
           <nav className="navbar navbar-expand-md navbar-light bg-light"> 
-            <p className="navbar-brand" >{brand}</p>        
+            <p className="navbar-brand" >{menuItems[2]}</p>        
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav mr-auto">
                 <li className="nav-item">
-                  <Link to={'/home'} className="nav-link">{main}</Link>
+                  <Link to={'/home'} className="nav-link">{menuItems[0]}</Link>
                 </li>
                 <li className="nav-item">
-                  <Link to={'/search'} className="nav-link">{search}</Link>
+                  <Link to={'/search'} className="nav-link">{menuItems[1]}</Link>
                 </li>
               </ul>
-              <span>
-                  <button  className="nav-link" onClick={this.switchLanguage}>{lang}</button>
-              </span>                           
+              <div className="switchLang" onClick={this.switchLanguage}>
+                  <button><img id="ru" width="30" src={Russian} alt="russian" /></button>
+                  <button><img id="by" width="30" src={Belarus} alt="russian" /></button>
+                  <button><img id="en" width="30" src={English}alt="russian" /></button>
+              </div>                           
             </div>
           </nav>
           <Switch>
               <Route exact path='/' component={mainPage}/>
               <Route exact path='/home' component={mainPage}/>
-              <Route exact path='/search' component={searchPage} />
+              <Route exact path='/search' component={searchPage}/>
           </Switch>
         </div>
       </Router>
     );
   }
 }
+
 export default App;
