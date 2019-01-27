a7f9688761b95f96baabac9c5c766bbc33ed4654
zmichgen <zmichgen@gmail.com>
2019-01-27 23:18:43 +0300

    feat: implement about module

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,37 +1,38 @@
-import React from 'react';
-import './about.scss';
+import React from "react";
+import "./about.scss";
+import Back from "./bel-mir.jpg";
 
 // data about project
 const screenData = {
   ru: {
-    title: 'Прикосниесь к прекрасному',
-    str1: 'На нашем портале Вы можете познакомится с писателями Беларуси',
-    str2: 'Классики или современники, стихи или проза',
-    str3: 'Сделай выбор...'
+    title: "Прикоснитесь к прекрасному",
+    str1: "На нашем портале Вы можете познакомится с писателями Беларуси",
+    str2: "Классики или современники, стихи или проза",
+    str3: "Сделай выбор..."
   },
   by: {
-    title: 'Дакраніцеся да прыгожага',
-    str1: 'На нашым партале Вы можаце пазнаёміцца з пісьменнікамі Беларусі',
-    str2: 'Класікі ці сучаснікі, вершы ці проза',
-    str3: 'Зрабі выбар'
+    title: "Дакраніцеся да прыгожага",
+    str1: "На нашым партале Вы можаце пазнаёміцца з пісьменнікамі Беларусі",
+    str2: "Класікі ці сучаснікі, вершы ці проза",
+    str3: "Зрабі выбар"
   },
   en: {
-    title: 'Touch the beautiful',
-    str1: 'On our portal you can get acquainted with the writers of Belarus',
-    str2: 'Classics or contemporaries, poems or prose',
-    str3: 'Make a choice'
+    title: "Touch the beautiful",
+    str1: "On our portal you can get acquainted with the writers of Belarus",
+    str2: "Classics or contemporaries, poems or prose",
+    str3: "Make a choice"
   }
-}
+};
 
-const InfoBlock = (props) => {
+const InfoBlock = props => {
   return (
-    <div className="about">
+    <div className="background">
       <h1>{screenData[props.lang].title}</h1>
       <h2>{screenData[props.lang].str1}</h2>
       <h2>{screenData[props.lang].str2}</h2>
       <h2>{screenData[props.lang].str3}</h2>
-    </div>    
+    </div>
   );
-}
+};
 
 export default InfoBlock;
