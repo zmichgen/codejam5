b9c91850ab9a736cb78f03bc85fc9d38c3f51462
zmichgen <zmichgen@gmail.com>
2019-01-28 10:03:50 +0300

    fix(footer, teem): (minor)change class to className

--- src/components/team/team.jsx
+++ src/components/team/team.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React from 'react';
 import "./team.scss";
 
@@ -31,72 +32,72 @@ const devTeam = {
 const Team = props => {
     return (
     <section>
-      <div class="developers-title">
+      <div className="developers-title">
         <h2>{devTeam[props.lang].title}</h2>
       </div>
-      <div class="developers" id='team'>
-        <div class='developers-teammate'>
-          <div class="teammate-contact noselect">
-            <div class="teammate-foto"></div>
+      <div className="developers" id='team'>
+        <div className='developers-teammate'>
+          <div className="teammate-contact noselect">
+            <div className="teammate-foto"></div>
             <span>{devTeam[props.lang].mdgh}</span>
             <span>Junior Javascript Developer</span>
             <div>
-              <a href="https://github.com/mdgh/" target="_blank"><i class="linked-icon">&#xe802;</i></a>
-              <a href="skype:dasha.mamchits?chat" target="_blank"><i class="linked-icon">&#xe804;</i></a>
-              <a href="https://t.me/darya_mamchyts" target="_blank"><i class="linked-icon">&#xf1d8;</i></a>
+              <a href="https://github.com/mdgh/" target="_blank"><i className="linked-icon">&#xe802;</i></a>
+              <a href="skype:dasha.mamchits?chat" target="_blank"><i className="linked-icon">&#xe804;</i></a>
+              <a href="https://t.me/darya_mamchyts" target="_blank"><i className="linked-icon">&#xf1d8;</i></a>
             </div>
           </div>
         </div>
-        <div class='developers-teammate'>
-          <div class="teammate-contact noselect">
-            <div class="teammate-foto"></div>
+        <div className='developers-teammate'>
+          <div className="teammate-contact noselect">
+            <div className="teammate-foto"></div>
             <span>{devTeam[props.lang].gaddyya}</span>
             <span>Junior Javascript Developer</span>
             <div>
-              <a href="https://github.com/gaddyya/" target="_blank"><i class="linked-icon">&#xe802;</i></a>
-              <a href="skype:gaddyya?chat" target="_blank"><i class="linked-icon">&#xe804;</i></a>
-              <a href="https://t.me/gaddyya" target="_blank"><i class="linked-icon">&#xf1d8;</i></a>
+              <a href="https://github.com/gaddyya/" target="_blank"><i className="linked-icon">&#xe802;</i></a>
+              <a href="skype:gaddyya?chat" target="_blank"><i className="linked-icon">&#xe804;</i></a>
+              <a href="https://t.me/gaddyya" target="_blank"><i className="linked-icon">&#xf1d8;</i></a>
             </div>
           </div>
         </div>
-        <div class='developers-teammate'>
-          <div class="teammate-contact noselect">
-            <div class="teammate-foto"></div>
+        <div className='developers-teammate'>
+          <div className="teammate-contact noselect">
+            <div className="teammate-foto"></div>
             <span>{devTeam[props.lang].zmichgen}</span>
             <span>Junior Javascript Developer</span>
             <div>
-              <a href="https://github.com/zmichgen/" target="_blank"><i class="linked-icon">&#xe802;</i></a>
-              <a href="skype:zmichgen?chat" target="_blank"><i class="linked-icon">&#xe804;</i></a>
-              <a href="https://t.me/zmichgen" target="_blank"><i class="linked-icon">&#xf1d8;</i></a>
+              <a href="https://github.com/zmichgen/" target="_blank"><i className="linked-icon">&#xe802;</i></a>
+              <a href="skype:zmichgen?chat" target="_blank"><i className="linked-icon">&#xe804;</i></a>
+              <a href="https://t.me/zmichgen" target="_blank"><i className="linked-icon">&#xf1d8;</i></a>
             </div>
           </div>
         </div>
-        <div class='developers-teammate'>
-          <div class="teammate-contact noselect">
-            <div class="teammate-foto"></div>
+        <div className='developers-teammate'>
+          <div className="teammate-contact noselect">
+            <div className="teammate-foto"></div>
             <span>{devTeam[props.lang].zerovers}</span>
             <span>Junior Javascript Developer</span>
             <div>
-              <a href="https://github.com/zerovers/" target="_blank"><i class="linked-icon">&#xe802;</i></a>
-              <a href="skype:Zeromon1111?chat" target="_blank"><i class="linked-icon">&#xe804;</i></a>
-              <a href="https://t.me/Zerover" target="_blank"><i class="linked-icon">&#xf1d8;</i></a>
+              <a href="https://github.com/zerovers/" target="_blank"><i className="linked-icon">&#xe802;</i></a>
+              <a href="skype:Zeromon1111?chat" target="_blank"><i className="linked-icon">&#xe804;</i></a>
+              <a href="https://t.me/Zerover" target="_blank"><i className="linked-icon">&#xf1d8;</i></a>
             </div>
           </div>
         </div>
-        <div class='developers-teammate'>
-          <div class="teammate-contact noselect">
-            <div class="teammate-foto"></div>
+        <div className='developers-teammate'>
+          <div className="teammate-contact noselect">
+            <div className="teammate-foto"></div>
             <span>{devTeam[props.lang].renardby} </span>
             <span>Junior Javascript Developer</span>
             <div>
-              <a href="https://github.com/RenardBY/" target="_blank"><i class="linked-icon">&#xe802;</i></a>
-              <a href="skype:renardby?chat" target="_blank"><i class="linked-icon">&#xe804;</i></a>
-              <a href="https://t.me/foxy_66" target="_blank"><i class="linked-icon">&#xf1d8;</i></a>
+              <a href="https://github.com/RenardBY/" target="_blank"><i className="linked-icon">&#xe802;</i></a>
+              <a href="skype:renardby?chat" target="_blank"><i className="linked-icon">&#xe804;</i></a>
+              <a href="https://t.me/foxy_66" target="_blank"><i className="linked-icon">&#xf1d8;</i></a>
             </div>
           </div>
         </div>
        </div>
-       <div class="developers-title">
+       <div className="developers-title">
        </div>
       </section>
   );
